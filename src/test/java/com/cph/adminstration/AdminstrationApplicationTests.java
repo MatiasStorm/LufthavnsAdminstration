6fd7ea9cdeba1cef65b4ed8d229438175f455e82
MatiasStorm <mat-435@hotmail.com>
2021-03-15 09:08:12 +0100

    Initial commit

---
+++ src/test/java/com/cph/adminstration/AdminstrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cph.adminstration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminstrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

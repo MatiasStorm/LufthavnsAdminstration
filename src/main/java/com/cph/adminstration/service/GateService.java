b8c594c7059397a81300bdde6311d83d2d67ae73
JooergenKoeldal <joer1894@stud.kea.dk>
2021-03-17 11:14:10 +0100

    add GateService

---
+++ src/main/java/com/cph/adminstration/service/GateService.java
@@ -0,0 +1,21 @@
+package com.cph.adminstration.service;
+
+import com.cph.adminstration.model.Gate;
+import com.cph.adminstration.repository.dao.GateDAO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GateService {
+    private final GateDAO gateDAO;
+
+
+    public GateService(GateDAO gateDAO) {
+        this.gateDAO = gateDAO;
+    }
+
+    public Gate create(Gate gate) {return gateDAO.create(gate);}
+
+    public List<Gate> readAll() {return  gateDAO.readAll();}
+}

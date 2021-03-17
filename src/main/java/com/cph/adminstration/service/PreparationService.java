92ea0fb5652374dc09283fd4aed8165292755c07
HolySupremeMaster <61865291+HolySupremeMaster@users.noreply.github.com>
2021-03-17 14:35:52 +0100

    Tests

--- src/main/java/com/cph/adminstration/service/PreparationService.java
+++ src/main/java/com/cph/adminstration/service/PreparationService.java
@@ -1,8 +1,12 @@
 package com.cph.adminstration.service;
 
+import com.cph.adminstration.model.Arrival;
 import com.cph.adminstration.model.Preparation;
 import com.cph.adminstration.repository.dao.PreparationDAO;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PreparationService {
@@ -11,6 +15,15 @@ public class PreparationService {
     public PreparationService(PreparationDAO preparationDAO) { this.preparationDAO = preparationDAO; }
 
     public Preparation create (Preparation preparation){ return preparationDAO.create(preparation); }
+
+    public List<Preparation> readAll() {
+        List<Preparation> preparations = new ArrayList<>();
+        for (Preparation preparation : preparationDAO.readAll()) {
+            preparations.add(preparation);
+
+        }
+        return preparations;
+    }
     }
 
 
--- src/test/java/com/cph/adminstration/service/PreparationServiceTest.java
+++ src/test/java/com/cph/adminstration/service/PreparationServiceTest.java
@@ -1,7 +1,19 @@
 package com.cph.adminstration.service;
 
+import com.cph.adminstration.model.Arrival;
+import com.cph.adminstration.model.Preparation;
+import com.cph.adminstration.repository.dao.PreparationDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,7 +21,31 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class PreparationServiceTest {
 
+@Mock
+    PreparationDAO dao;
+    PreparationService service;
+
+    @BeforeEach
+    public void setup(){
+        this.service = new PreparationService(dao);
+    }
+
+    @Test
+    void create() {
+        Preparation preparation = new Preparation();
+        service.create(preparation);
+        Mockito.verify(dao, Mockito.times(1)).create(preparation);
+    }
+    @Test
+    void readAll() {
+        service.readAll();
+        Mockito.verify(dao, Mockito.times(1)).readAll();
+    }
 
 
 
 }
+
+
+
+

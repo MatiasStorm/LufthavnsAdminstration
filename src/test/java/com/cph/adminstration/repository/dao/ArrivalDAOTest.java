dd2d74c97cb581532618d483ec3507b343b8ec9e
MatiasStorm <mat-435@hotmail.com>
2021-03-17 10:22:16 +0100

    Add departure and arrival mapper test

---
+++ src/test/java/com/cph/adminstration/repository/dao/ArrivalDAOTest.java
@@ -0,0 +1,16 @@
+package com.cph.adminstration.repository.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrivalDAOTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void readAll() {
+    }
+}

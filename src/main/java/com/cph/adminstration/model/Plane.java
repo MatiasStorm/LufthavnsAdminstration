ccc6a04545bda0211b0dd63da195fa7690f42642
JooergenKoeldal <joer1894@stud.kea.dk>
2021-03-18 09:36:10 +0100

    add model class plane

--- src/main/java/com/cph/adminstration/model/Plane.java
+++ src/main/java/com/cph/adminstration/model/Plane.java
@@ -1,9 +1,32 @@
 package com.cph.adminstration.model;
 
 public class Plane {
+
     private int iata;
     private String type;
     private char planeSize;
 
+    public int getIata() {
+        return iata;
+    }
 
+    public void setIata(int iata) {
+        this.iata = iata;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public char getPlaneSize() {
+        return planeSize;
+    }
+
+    public void setPlaneSize(char planeSize) {
+        this.planeSize = planeSize;
+    }
 }

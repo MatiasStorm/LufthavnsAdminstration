46cc4ff966d420fc858367012bbeca4e7cece198
PVGlimoe <phillip.glimoe@hotmail.com>
2021-03-18 11:38:32 +0100

    init

---
+++ src/main/java/com/cph/adminstration/model/PlaneParking.java
@@ -0,0 +1,20 @@
+package com.cph.adminstration.model;
+
+import java.util.ArrayList;
+
+public class PlaneParking {
+
+    private static ArrayList<Boolean> gateIsVacant;
+
+
+
+    public int findGate(Plane plane, ArrayList<Gate> gates){
+        int vacantGateNumber = 0;
+        for (Gate gate: gates) {
+            if (gate.getGateSize())
+        }
+        return vacantGateNumber;
+    }
+
+
+}
